790e40f2c489e0f2196cdcb2f46f664bddeb7e95
Queror254 <mwendavictorm@gmail.com>
2024-11-11 23:30:32 +0300

    ---

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,6 +12,7 @@ def books_in_library(library_name):
     return library.books.all()
 
 # Retrieve the librarian for a library
-def librarian_for_library(library_name):
+def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Librarian.objects.get(library=library)
+    return librarian
