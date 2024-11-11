df17657e3f1b8388489b3fde1b5b727306fc735d
Queror254 <mwendavictorm@gmail.com>
2024-11-11 23:47:07 +0300

    -_-_

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='relationship_app/logout'),
 
     path('admin/', admin_view, name='admin_view'), # type: ignore
     path('librarian/', librarian_view, name='librarian_view'),# type: ignore
