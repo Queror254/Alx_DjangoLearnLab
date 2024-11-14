3d8ca92fb76d75d59dd3a3d738b4826419e1e8f9
Queror254 <mwendavictorm@gmail.com>
2024-11-14 19:09:12 +0300

    add auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from .views import list_books
 from .views import LibraryDetailView
+from .views import login_view
+from .views import logout_view
+from .views import register
 from django.urls import path
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register, name='register'),
     path('books/', list_books, name='list_books'),
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import Book
 from .models import Library
 
 # Create your views here.
+# User Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# User Login view
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the home page or other page
+        else:
+            return render(request, 'relationship_app/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'relationship_app/login.html')
+
+# User Logout view
+@login_required 
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+
 def home(request):
     return render(request, 'home.html')
 
