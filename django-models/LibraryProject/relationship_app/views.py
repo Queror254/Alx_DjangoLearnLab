04076b37469279e964358a195d31de713ee19c1a
Queror254 <mwendavictorm@gmail.com>
2024-11-14 18:36:03 +0300

    add new views list_books & LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    models = Library
+    template_name = 'library_books.html'
+    context_object_name = 'library'
--- django-models/core/urls.py
+++ django-models/core/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='Home'),
+    path('books/', views.list_books, name='list_books'),  # Function-based view for listing all books
+    path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
