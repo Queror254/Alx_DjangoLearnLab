e5e48b71f958f741ab8323638fcedfd60ae5597b
Queror254 <mwendavictorm@gmail.com>
2024-12-18 21:48:20 +0300

    add config for db

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_api',
+        'USER': 'root',              
+        'PASSWORD': '',              
+        'HOST': '127.0.0.1',         
+        'PORT': '3306',              
     }
 }
 
